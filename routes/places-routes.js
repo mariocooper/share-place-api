74d8dd6c485ea4e5f6a3e7082d30f798b055158f
Mario Cooper <mariocooper00@hotmail.com>
2023-09-21 18:06:18 +0100

    added Place specific routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET request in Places');
-  res.json({ message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "The world's most magnificent Art Deco skyscraper",
+    location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+    },
+    address: "20 W 34th St., New York, NY 10001, USA",
+    creator: "u1"
+  },
+]
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  })
+  res.json({place});
 });
 
 module.exports = router;
