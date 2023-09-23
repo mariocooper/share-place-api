0c96adaf552ab842eea9673c617c4bd8fe108151
Mario Cooper <mariocooper00@hotmail.com>
2023-09-23 22:50:28 +0100

    added validation to routes

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const uuid = require('uuid');
+const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
 
@@ -46,6 +47,12 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please try again', 422);
+  }
+  
   const { title, description, location, address, creator } = req.body;
   // const title = req.body.title
   const createdPlace = {
@@ -63,6 +70,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please try again', 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -78,6 +91,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('A place with the given ID does not exist', 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
   res.status(200).json({ message: 'Place deleted' });
 };
