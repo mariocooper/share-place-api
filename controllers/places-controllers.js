0d1d7119a6ba76f191624d9bbac6578dc3880e03
Mario Cooper <mariocooper00@hotmail.com>
2023-09-29 13:53:33 +0100

    added populate function to getUsers

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -48,9 +48,9 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
   
-  let places;
+  let userWithPlaces;
   try {
-    places = await Place.find({ creator: userId });
+    userWithPlaces = await User.findById(userId).populate('places');
   } catch (err) {
     const error = new HttpError(
       'Something went wrong, could not find a place with that user ID',
@@ -59,7 +59,7 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
       const error = new HttpError(
         'Could not find places for the provided user ID',
         404
@@ -67,7 +67,7 @@ const getPlacesByUserId = async (req, res, next) => {
       return next(error);
   }
     
-  res.json({ places: places.map(place => place.toObject({ getters: true })) });
+  res.json({ places: userWithPlaces.places.map(place => place.toObject({ getters: true })) });
 };
 
 const createPlace = async (req, res, next) => {
