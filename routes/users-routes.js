25f5781fa8001dc84cdd84968f3a7f5ebbd902dd
Mario Cooper <mariocooper00@hotmail.com>
2023-10-04 21:56:55 +0100

    adding use of multer package to save files

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersControllers = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
 
 router.post(
-  '/signup', 
+  '/signup',
+  fileUpload.single('image'),
   [
     check('name')
       .not()
