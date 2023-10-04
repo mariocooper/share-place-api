5703342476366024f1f8184441bd4aeb2696e024
Mario Cooper <mariocooper00@hotmail.com>
2023-10-04 22:00:16 +0100

    adding filtering to file uploads

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -14,10 +14,15 @@ const fileUpload = multer({
       cb(null, 'uploads/images');
     },
     filename: (req, file, cb) => {
-      const ext = MIME_TYPE_MAP[file.mimetype];
+      const ext = !!MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid.v4() + '.' + ext);
     }
-  })
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type');
+    cb(error, isValid);
+  }
 });
 
 module.exports = fileUpload;
